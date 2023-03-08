70fc3b9070f4df911788aa7e27601fcc725445b6
Calle Luks <calle@luks.in>
2023-03-08 11:17:24 +0100

    Clear out everything

--- config/initializers/session_store.rb
+++
@@ -1,3 +0,0 @@
-Rails.application.config.session_store :active_record_store
-
-ActiveRecord::SessionStore::Session.serializer = :null
--- config/routes.rb
+++
@@ -1,8 +0,0 @@
-Rails.application.routes.draw do
-  root 'pages#home'
-
-  post 'sign_in' => 'sessions#init'
-  get 'callback' => 'sessions#create'
-  post 'sign_out' => 'sessions#destroy'
-  post 'backchannel_logout' => 'sessions#backchannel_logout'
-end
