6b24c0fefa7857628c12f9530ec87c0f0dbe8654
Calle Luks <calle@luks.in>
2023-03-08 14:04:11 +0100

    Build an example news site with Sinatra

---
+++ Gemfile
@@ -0,0 +1,9 @@
+source 'https://rubygems.org'
+
+ruby '3.2.0'
+
+# Web application DSL
+gem 'sinatra'
+
+# Web server
+gem 'puma'
---
+++ server.rb
@@ -0,0 +1,25 @@
+require 'bundler/setup'
+require 'json'
+require 'sinatra'
+
+ARTICLES = JSON.parse(File.read(File.join(__dir__, 'articles.json')), object_class: OpenStruct)
+
+get '/' do
+  @articles = ARTICLES
+
+  erb :home
+end
+
+get '/articles/:slug' do
+  @article = ARTICLES.find { |article| article.slug == params[:slug] }
+
+  raise Sinatra::NotFound unless @article
+
+  erb :article
+end
+
+helpers do
+  def simple_format(text)
+    text.split(/\n\n+/).map { |paragraph| "<p>#{paragraph}</p>" }.join("\n")
+  end
+end
