70fc56c72550dcc046be31171d177a3a92cd4286
Calle Luks <calle@pliro.co>
2024-10-10 11:27:53 +0200

    Use REDIS_URL instead of REDIS_TLS_URL
    REDIS_URL is now set to a TLS rediss:// URL:
    
    https://help.heroku.com/45F0AY28/what-are-the-upcoming-changes-to-the-redis_url-config-var-for-heroku-data-for-redis-mini-add-ons

--- server.rb
+++ server.rb
@@ -21,7 +21,7 @@ PLIRO_CLIENT_ID = ENV.fetch('PLIRO_CLIENT_ID')
 PLIRO_CLIENT_SECRET = ENV.fetch('PLIRO_CLIENT_SECRET')
 
 # Redis is used to store active Pliro session IDs:
-$redis = Redis.new(url: ENV['REDIS_TLS_URL'], ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE })
+$redis = Redis.new(url: ENV['REDIS_URL'], ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE })
 
 # Sessions expire automatically after 7 days of inactivity:
 SESSION_EXPIRATION_TIME = 7 * 24 * 60 * 60 # seconds
